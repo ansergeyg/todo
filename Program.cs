0baeaa12fd49ef3279e28379eccd521a38e1762f
Sergey An <ansergeyg@gmail.com>
2024-09-19 18:27:06 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+// First, we added this comment for testing merging squash PR that we will do right after this change is merged.
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
